04e67f06ba80e2bb2ca5bff0c389f089fef8bf19
Raven Earl Nacua <ravennacua2@gmail.com>
2025-07-16 18:05:40 +0800

    added SHORTEST REMAINNG TIME

--- OS.java
+++ OS.java
@@ -1,4 +1,3 @@
-
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
@@ -173,16 +172,26 @@ public class OS {
                 int priority = Integer.parseInt(tableModel.getValueAt(i, 4).toString());
                 processes.add(new Process(i + 1, name, arrival, burst, priority));
             }
-            if (selected.equals("First In First Out (FIFO)")) {
-                selectionMessage.setText("Running: FIRST IN FIRST OUT (FIFO)");
-                runFCFS(processes);
-            } else if (selected.equals("Shortest Job First (SJF)")) {
-                selectionMessage.setText("Running: SHORTEST JOB FIRST (SJF)");
-                runSJF(processes);
-            } else if (selected.equals("Round Robin")) {
-                int quantum = timeQuantumSlider.getValue();
-                JOptionPane.showMessageDialog(frame, "Selected Round Robin with Quantum = " + quantum);
-            } else JOptionPane.showMessageDialog(frame, "Please select a valid algorithm.");
+            switch (selected) {
+                case "First In First Out (FIFO)":
+                    selectionMessage.setText("Running: FIRST IN FIRST OUT (FIFO)");
+                    runFCFS(processes);
+                    break;
+                case "Shortest Job First (SJF)":
+                    selectionMessage.setText("Running: SHORTEST JOB FIRST (SJF)");
+                    runSJF(processes);
+                    break;
+                case "Shortest Remaining Time First (SRTF)":
+                    selectionMessage.setText("Running: SHORTEST REMAINING TIME FIRST (SRTF)");
+                    runSRTF(processes);
+                    break;
+                case "Round Robin":
+                    int quantum = timeQuantumSlider.getValue();
+                    JOptionPane.showMessageDialog(frame, "Selected Round Robin with Quantum = " + quantum);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(frame, "Please select a valid algorithm.");
+            }
         });
 
         frame.setVisible(true);
@@ -203,7 +212,7 @@ public class OS {
         }
     }
 
-    public static void runFCFS(List<Process> list) {
+   public static void runFCFS(List<Process> list) {
         ganttContainer.removeAll();
         list.sort(Comparator.comparingInt(p -> p.arrivalTime));
         new Thread(() -> {
@@ -344,5 +353,83 @@ public class OS {
             });
         }).start();
     }
-}
 
+    public static void runSRTF(List<Process> processes) {
+        ganttContainer.removeAll();
+        new Thread(() -> {
+            int time = 0, completed = 0, n = processes.size();
+            int totalTAT = 0, totalWT = 0;
+            boolean[] isCompleted = new boolean[n];
+
+            while (completed < n) {
+                int minRemaining = Integer.MAX_VALUE, idx = -1;
+                for (int i = 0; i < n; i++) {
+                    Process p = processes.get(i);
+                    if (p.arrivalTime <= time && !isCompleted[i] && p.remainingTime < minRemaining && p.remainingTime > 0) {
+                        minRemaining = p.remainingTime;
+                        idx = i;
+                    }
+                }
+
+                if (idx == -1) {
+                    time++;
+                    continue;
+                }
+
+                Process current = processes.get(idx);
+                if (current.remainingTime == current.burstTime) {
+                    current.responseTime = time - current.arrivalTime;
+                }
+
+                JLabel label = new JLabel(current.name);
+                label.setPreferredSize(new Dimension(40, 40));
+                label.setHorizontalAlignment(SwingConstants.CENTER);
+                label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                SwingUtilities.invokeLater(() -> {
+                    ganttContainer.add(label);
+                    ganttContainer.revalidate();
+                    ganttContainer.repaint();
+                });
+
+                current.remainingTime--;
+                int progress = (int)(((double)(current.burstTime - current.remainingTime) / current.burstTime) * 100);
+                int remaining = current.remainingTime;
+                final int row = idx;
+                SwingUtilities.invokeLater(() -> {
+                    statusModel.setValueAt("Running", row, 1);
+                    statusModel.setValueAt(progress + "%", row, 2);
+                    statusModel.setValueAt(remaining, row, 3);
+                });
+
+                try {
+                    double speed = speedSlider.getValue() / 10.0;
+                    Thread.sleep((long)(500 / speed));
+                } catch (Exception e) { e.printStackTrace(); }
+
+                if (current.remainingTime == 0) {
+                    current.completionTime = time + 1;
+                    current.turnaroundTime = current.completionTime - current.arrivalTime;
+                    current.waitingTime = current.turnaroundTime - current.burstTime;
+                    isCompleted[idx] = true;
+                    completed++;
+                    SwingUtilities.invokeLater(() -> {
+                        statusModel.setValueAt("Completed", row, 1);
+                        statusModel.setValueAt("100%", row, 2);
+                        statusModel.setValueAt(0, row, 3);
+                        statusModel.setValueAt(current.waitingTime, row, 4);
+                    });
+                }
+                time++;
+            }
+
+            final float avgTAT = (float) totalTAT / n;
+            final float avgWT = (float) totalWT / n;
+            final int finalExecTime = time;
+            SwingUtilities.invokeLater(() -> {
+                avgTurnaroundLabel.setText("Average Turnaround: " + String.format("%.2f", avgTAT));
+                avgWaitingLabel.setText("Average Waiting: " + String.format("%.2f", avgWT));
+                totalExecLabel.setText("Total Execution Time: " + finalExecTime);
+            });
+        }).start();
+    }
+}
